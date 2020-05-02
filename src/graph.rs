1abcf2ccf964c464445053ced2e9dcf775dc6481
Andrey Sokolov <andsok75@fastmail.com>
2020-05-02 18:08:57 +1000

    applied formatting

--- src/graph.rs
+++ src/graph.rs
@@ -34,32 +34,52 @@ impl<Data: Debug, Props: Debug> Graph<Data, Props> {
             props: Vec::new(),
         }
     }
-    pub fn node(self: &Self, node_id: usize) -> &Node { &self.nodes[node_id] }
-    pub fn data(self: &Self, node_id: usize) -> &Data { &self.data[node_id] }
-    pub fn edge(self: &Self, edge_id: usize) -> &Edge { &self.edges[edge_id] }
-    pub fn props(self: &Self, edge_id: usize) -> &Props { &self.props[edge_id] }
+    pub fn node(self: &Self, node_id: usize) -> &Node {
+        &self.nodes[node_id]
+    }
+    pub fn data(self: &Self, node_id: usize) -> &Data {
+        &self.data[node_id]
+    }
+    pub fn edge(self: &Self, edge_id: usize) -> &Edge {
+        &self.edges[edge_id]
+    }
+    pub fn props(self: &Self, edge_id: usize) -> &Props {
+        &self.props[edge_id]
+    }
 
     pub fn insert_node(self: &mut Self, data: Data) -> usize {
         let new_node_id = self.nodes.len();
-        self.nodes.push(Node { id: new_node_id, incoming: Vec::new(), outgoing: Vec::new() });
+        self.nodes.push(Node {
+            id: new_node_id,
+            incoming: Vec::new(),
+            outgoing: Vec::new(),
+        });
         self.data.push(data);
         return new_node_id;
     }
     pub fn insert_edge(self: &mut Self, from: usize, to: usize, props: Props) -> usize {
         let new_edge_id = self.edges.len();
-        self.edges.push(Edge { id: new_edge_id, from: from, to: to });
+        self.edges.push(Edge {
+            id: new_edge_id,
+            from: from,
+            to: to,
+        });
         self.props.push(props);
         self.nodes[from].outgoing.push(new_edge_id);
         self.nodes[to].incoming.push(new_edge_id);
         return new_edge_id;
     }
     pub fn json(&self) -> String
-        where Data: Display, Props: Display
+    where
+        Data: Display,
+        Props: Display,
     {
         let mut result = "{\"nodes\":[".to_string();
         for id in 0..self.nodes.len() {
             result += &format!("{}", self.data[id]);
-            if id < self.nodes.len()-1 { result += &","; }
+            if id < self.nodes.len() - 1 {
+                result += &",";
+            }
         }
         result += &"],";
         result += &"\"edges\":[";
@@ -68,8 +88,13 @@ impl<Data: Debug, Props: Debug> Graph<Data, Props> {
             let props = &self.props[id];
             let from = &self.nodes[edge.from];
             let to = &self.nodes[edge.to];
-            result += &format!("{{\"from\":{},\"to\":{},\"props\":{}}}", self.data[from.id], self.data[to.id], props);
-            if id < self.edges.len()-1 { result += &","; }
+            result += &format!(
+                "{{\"from\":{},\"to\":{},\"props\":{}}}",
+                self.data[from.id], self.data[to.id], props
+            );
+            if id < self.edges.len() - 1 {
+                result += &",";
+            }
         }
         result += &"]}";
         result
@@ -88,7 +113,7 @@ pub fn sample() -> Graph<SampleData, SampleProps> {
         let from = (rand::random::<u8>() / 10) as usize;
         let to = (rand::random::<u8>() / 10) as usize;
         let cost = rand::random::<u8>();
-        graph.insert_edge(from, to, SampleProps{ cost: cost });
+        graph.insert_edge(from, to, SampleProps { cost: cost });
     }
     return graph;
 }
