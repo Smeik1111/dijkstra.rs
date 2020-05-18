f8df780bf6723876671828235ae8520a0ed88158
Andrey Sokolov <andsok75@fastmail.com>
2020-05-18 15:36:25 +1000

    clap

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,16 @@
+use clap::{App, Arg};
 use serde::{Deserialize, Serialize};
-use std::cmp::min;
-use std::collections::HashMap;
-use std::fs::File;
-use std::path::Path;
 
-use dijkstra::graph::Graph;
-
-const N: u8 = 10;
-const NUM_NODES: usize = 1000;
-type Position = (u8, u8, u8);
+use dijkstra::graph::*;
 
 fn main() {
-    // 3d grid with N nodes along each dimension and directed edges to all neighbours
-    let mut graph = Graph::new();
-    for _ in 0..NUM_NODES {
-        graph.insert_node(State {});
-    }
-    for id in 0..graph.num_nodes() {
-        for neighbour in neighbours(position_from(id)) {
-            let cost = rand::random::<u8>();
-            if cost < 200 {
-                // skip the majority of the possible edges
-                continue;
-            }
-            graph.insert_edge(id, id_from(neighbour), Props { cost: cost - 200 });
-        }
-    }
-    let json = serde_json::to_string(&graph).expect("failed to serialise generated graph");
-    println!("{}", json);
-    let source = id_from((1, 1, 1));
-    let targets = vec![
-        id_from((N - 2,N - 1, N - 1)),
-        id_from((N - 1,N - 2, N - 1)),
-        id_from((N - 1,N - 1, N - 2)),
-    ];
-    if let Some(path) = graph.best_path(source, &targets) {
-        eprintln!("{:?}", path);
-        eprintln!("{:?}", graph.cost(&path));
-    }
+    let (source, targets) = args();
     let graph: Graph<State, Props> =
-       serde_json::from_str(&json).expect("failed to deserialise generated graph");
-}
-
-fn id_from(position: Position) -> usize {
-    let i = position.0 as usize;
-    let j = position.1 as usize;
-    let k = position.2 as usize;
-    let n = N as usize;
-    n * (n * i + j) + k
-}
-
-fn position_from(id: usize) -> Position {
-    let n = N as usize;
-    let k = (id % n) as u8;
-    let j = ((id / n) % n) as u8;
-    let i = (id / n / n) as u8;
-    (i, j, k)
-}
-
-// loopy edges on the sides are allowed
-fn neighbours(position: Position) -> Vec<Position> {
-    let mut result = Vec::new();
-    let range = |index: u8| [index.saturating_sub(1), min(index + 1, N - 1)];
-    for &i in range(position.0).iter() {
-        for &j in range(position.1).iter() {
-            for &k in range(position.2).iter() {
-                result.push((i, j, k));
-            }
-        }
+        serde_json::from_reader(std::io::stdin()).expect("failed to deserialise graph");
+    if let Some(path) = graph.best_path(source, &targets) {
+        println!("path: {:?}", path);
+        println!("cost: {:?}", graph.cost(&path) as u64);
     }
-    result
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -80,8 +21,39 @@ struct Props {
     cost: u8,
 }
 
-impl dijkstra::graph::Cost for Props {
+impl Cost for Props {
     fn cost(&self) -> f64 {
         self.cost as f64
     }
+}
+
+fn args() -> (NodeId, Vec<NodeId>) {
+    let matches = App::new("Dijkstra search")
+        .arg(
+            Arg::with_name("source")
+                .long("source")
+                .help("Source node id")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("targets")
+                .long("targets")
+                .help("Target node id(s) (comma separated)")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
+    let source = matches
+        .value_of("source")
+        .unwrap()
+        .parse::<usize>()
+        .expect("failed to parse source");
+    let targets = matches
+        .value_of("targets")
+        .unwrap()
+        .split(",")
+        .map(|target| target.parse::<usize>().expect("failed to parse targets"))
+        .collect::<Vec<usize>>();
+    (source, targets)
 }
