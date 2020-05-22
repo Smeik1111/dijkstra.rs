9dd83faf9125b6cab6309d3e05e17bf9d6e56f92
Andrey Sokolov <andsok75@fastmail.com>
2020-05-22 14:45:57 +1000

    test_grid3d minor shuffle

--- tests/test_grid3d.rs
+++ tests/test_grid3d.rs
@@ -1,10 +1,51 @@
 use serde::{Deserialize, Serialize};
-use std::fs;
-use std::path::Path;
 use std::{thread, time};
 use std::cmp::min;
 
 use dijkstra::graph::{Advance, Graph, NodeId};
+
+#[test]
+fn make_grid3d() {
+    let graph = grid3d();
+    let json = serde_json::to_string(&graph).expect("failed to serialise generated graph");
+    let graph: Graph<State, Props> =
+        serde_json::from_str(&json).expect("failed to deserialise generated graph");
+    assert_eq!(graph.num_nodes(), 1000);
+    assert_eq!(graph.num_edges(), 6_000);
+}
+
+// 3d grid with N nodes along each dimension, where each node is connected to all neighbours
+const N: usize = 10;
+
+fn grid3d() -> Graph<State, Props> {
+    let mut graph: Graph<State, Props> = Graph::new();
+    let node_ids = (0..N.pow(3))
+        .map(|_| graph.insert_node(State { cost: None}))
+        .collect::<Vec<_>>();
+    for from in node_ids {
+        for to in neighbours(from) {
+            let cost = rand::random::<u8>();
+            graph.insert_edge(from, to, Props { cost: cost });
+        }
+    }
+    graph
+}
+
+fn neighbours(id: NodeId) -> Vec<NodeId> {
+    let position_of = |id| (id % N, (id / N) % N, id / N / N);
+    let id_of = |i, j, k| i + N * (j + N * k);
+    let less = |index: usize| index.saturating_sub(1);
+    let more = |index: usize| min(index + 1, N - 1);
+    let (i, j, k) = position_of(id);
+    vec![
+        id_of(less(i), j, k),
+        id_of(more(i), j, k),
+        id_of(i, less(j), k),
+        id_of(i, more(j), k),
+        id_of(i, j, less(k)),
+        id_of(i, j, more(k)),
+    ]
+}
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 struct State {
@@ -30,42 +71,4 @@ impl Advance<State, Props> for State {
     fn cost(&self) -> Option<f64> {
         self.cost
     }
-}
-
-// 3d grid with N nodes along each dimension, where each node is connected to all neighbours
-const N: usize = 10;
-
-#[test]
-fn make_grid3d() {
-    let mut graph: Graph<State, Props> = Graph::new();
-    let node_ids = (0..N.pow(3))
-        .map(|_| graph.insert_node(State { cost: None}))
-        .collect::<Vec<_>>();
-    for from in node_ids {
-        for to in neighbours(from) {
-            let cost = rand::random::<u8>();
-            graph.insert_edge(from, to, Props { cost: cost });
-        }
-    }
-    let json = serde_json::to_string(&graph).expect("failed to serialise generated graph");
-    let graph: Graph<State, Props> =
-        serde_json::from_str(&json).expect("failed to deserialise generated graph");
-    assert_eq!(graph.num_nodes(), 1000);
-    assert_eq!(graph.num_edges(), 6_000);
-}
-
-fn neighbours(id: NodeId) -> Vec<NodeId> {
-    let position_of = |id| (id % N, (id / N) % N, id / N / N);
-    let id_of = |i, j, k| i + N * (j + N * k);
-    let less = |index: usize| index.saturating_sub(1);
-    let more = |index: usize| min(index + 1, N - 1);
-    let (i, j, k) = position_of(id);
-    vec![
-        id_of(less(i), j, k),
-        id_of(more(i), j, k),
-        id_of(i, less(j), k),
-        id_of(i, more(j), k),
-        id_of(i, j, less(k)),
-        id_of(i, j, more(k)),
-    ]
 }
